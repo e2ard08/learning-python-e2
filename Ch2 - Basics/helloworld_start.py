4e9c29567fb0296b21a15800947deae622e3a121
e2ard <edward.8805@gmail.com>
2024-01-15 22:40:03 -0600

    update 1.0

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,10 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+   print("Hello Wolrd!")
+   name = input ("What is your name?")
+   print("Nice to meet you" , name)
 
+if __name__ == "_main_":
+  main()
